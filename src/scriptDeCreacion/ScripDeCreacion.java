9a922c1c6d8d45c4657772cff09688ba1d00eb41
Pablo <pablo.izqcac@sanviatorvalladolid.com>
2025-02-07 11:09:58 +0100

    Incio del proyecto

---
+++ src/scriptDeCreacion/ScripDeCreacion.java
@@ -0,0 +1,40 @@
+package scriptDeCreacion;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ScripDeCreacion {
+
+	public static void main(String[] args) {
+		generadorId();
+
+	}
+	public static void generadorId() {
+		
+		int[] id = new int[200];
+		int numero = 0;
+		
+		for (int i = 0 ; i<id.length ; i++) {
+			
+			Arrays.copyOf(id, id.length+1);
+			
+			id[id.length-1] = numero;
+			numero++;
+		
+		}
+	}
+	public static void comerciales() {
+		
+		//200 nombres
+		String[] nombres = {"DeliManía","MundoBebé","SaboresDelicia","ElectroMax","VivaEstilo","AutoNova","DulceCielo","VivaNaturaleza","TuCasaIdeal","SolucionesRápidas","MágicoReloj","EnigmaTecnológico","PlanetaVerde","EcoVibes","EstiloUrbano","DúoPerfecto","LunaViva","BrisaFresca","KiloSmart","TopOportunidades","PinturasMágicas","VueltaAlColección","LíneaRápida","ColorSplash","SúperFácil","RápidoYJusto","FitnessPro","JuguetesMágicos","ComidaDeliciosa","GoVerde","Salud360","ViajePerfecto","TuFuturoOnline","RelojPerfecto","MundoAmigos","AventuraPlus","TiemposFelices","EstiloElegante","¡VamosRápido!","ExperienciaÚnica","VelocidadPura","TodoEnUno","EcoRápido","MasterClases","SolucionesInstantáneas","LaFábricaDeSueños","MundoFuturo","MagiaEnCasa","MiVidaSana","SuperEnergía","LíneaDorada","VuelaAlto","MundoVirtual","TuMejorMomento","TodoPorTi","SolucionesPerfectas","MenteFeliz","ReposoTotal","CaféMágico","TuProgreso","EstiloEnCasa","TuÉxitoSeguro","JuntosEsMejor","SolucionesPro","MundoDeSueños","EstiloFresco","ComidaExpress","HogarDulceHogar","TiempoDeOportunidades","BrillaAhora","MundoFeliz","FórmulaMágica","RápidoYSeguro","AuténticoSabor","CrecimientoFácil","TuFuerza","MomentosPerfectos","CuidadoTotal","SaborReal","RápidoYNuevo","SolucionesAmigables","BienestarInmediato","HéroesEnCasa","MentePositiva","DíasFelices","CaféVital","EnergíaIntensa","HogarBrillante","ConectadosSiempre","¡RápidoAlÉxito!","MundoGourmet","VelocidadTotal","PlanetaMagia","EstiloClaro","EquilibrioPerfecto","TuOportunidad","SaborExclusivo","ComodidadTotal","DesafíoEnCasa","MañanasFelices","MenteCreativa","ÁgilComercio","NuevaEra","EquilibrioRápido","SolucionesExpress","MundoFresco","TodoParaTi","BienestarSeguro","VibranteEstilo","FamiliaFeliz","ComidaViva","TodoPerfecto","RápidoYFresco","JuguetesEstrella","AventuraFamiliar","RecetasFáciles","EfectoRápido","ViajeSeguro","EstiloVibrante","SolucionesIdeales","SaborNatural","ElectroNuevo","InnovaciónRápida","EnergíaVital","TuCasaFácil","MundoImparable","PlanetaFeliz","BrisaTotal","LlamaAventura","LucesBrillantes","DiversiónTotal","FábricaDeIdeas","ExperienciaInigualable","SolucionesIncreíbles","EscapadaIdeal","EstiloFácil","ComerSaludable","MagiaDeViaje","!RápidoYFácil","HogarNatural","EcoLuz","CieloFresco","VelocidadTotal","¡VamosALoGrande!","TuNuevoComienzo","ImaginaMás","ViajeMágico","NuevaPerspectiva","SonrisaSana","SolucionesGeniales","MundoMaravilloso","BrillaConFuerza","TodoPerfectoParaTi","NaturalezaEnCasa","ViajeIdeal","MomentosMágicos","EstiloPuro","ComidaPlena","SoloLoMejor","SuperSalud","ConectadosSiempre","EstiloInconfundible","MundoActivo","¡DisfrutaMás!","BellezaRadiante","MaravillasDiarias","JuguetesFeliz","EnergíaImparable","VibranteEstilo","RápidoYSeguro","CuidadoTotal","HogarIdeal","SolucionesVivas","CaféMaravilloso","EstiloUnico","UniversoSalud","TuTiempoFeliz","RecetasFrescas","EcoInnovador","FuerzaVital","TodoEstáBien","MagiaDeEstilo","EficienciaTotal","¡SéUnHéroe!","SonrisaRápida","MundoSostenible","EstiloGourmet","CrecimientoNatural","ComodidadFácil","InnovaciónDiaria","ViajePerfecto","CasaFeliz","SoloParaTi","JuguetesGeniales","MagiaViva","BrillaConEstilo","DescubreMás","ViajeIncreíble","EncuentraTodo"};
+		
+		String[] categorias = {"Resdencial","Comerciale","De Terreno","De Corporativas","De Renta","Internacional"};
+		
+		String pais = "España";
+		
+	
+		
+		
+	}
+	
+}
