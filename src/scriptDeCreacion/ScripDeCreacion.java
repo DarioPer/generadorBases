4f83f4130b228514458a0760259e1ce9dfcd37df
dario <dario.perbar@sanviator.com>
2025-02-11 13:17:06 +0100

    creado linea propia

--- src/scriptDeCreacion/ScripDeCreacion.java
+++ src/scriptDeCreacion/ScripDeCreacion.java
@@ -1,13 +1,39 @@
 package scriptDeCreacion;
 
+import java.text.DecimalFormat;
+import java.text.Format;
 import java.util.Arrays;
 import java.util.Random;
 
 public class ScripDeCreacion {
 
 	public static void main(String[] args) {
-		generadorId();
+		principal();
 
+		/*
+		 * import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        // Número entero aleatorio
+        int numeroAleatorio = random.nextInt();
+        System.out.println("Número entero aleatorio: " + numeroAleatorio);
+
+        // Número entero aleatorio en el rango 0-99
+        int numeroAleatorioRango = random.nextInt(100);
+        System.out.println("Número entero aleatorio en el rango 0-99: " + numeroAleatorioRango);
+
+        // Número decimal aleatorio entre 0.0 y 1.0
+        double numeroAleatorioDecimal = random.nextDouble();
+        System.out.println("Número decimal aleatorio entre 0.0 y 1.0: " + numeroAleatorioDecimal);
+
+        // Número decimal aleatorio en el rango 0.0-10.0
+        double numeroAleatorioRangoDecimal = 10.0 * random.nextDouble();
+        System.out.println("Número decimal aleatorio en el rango 0.0-10.0: " + numeroAleatorioRangoDecimal);
+    }
+		 */
 	}
 	public static void generadorId() {
 		
@@ -23,16 +49,52 @@ public class ScripDeCreacion {
 		
 		}
 	}
-	public static void comerciales() {
+
+	public static String generarDni(){
+		DecimalFormat formato = DecimalFormat.	
+
+
+	}
+
+	public static void Inmoviliaria(){
+		Random random=new Random();
+
+		String[] nombreInmoviliaria = {};
+		String[] categorias = {"Regional","Nacional","Provincial","Internacional"};
+		String paisSede = "España";
+		/*nº oficinas */ 
+
+
+	}
+
+
+
+
+
+
+	public static void principal() {
 		
-		//200 nombres
-		String[] nombres = {"DeliManía","MundoBebé","SaboresDelicia","ElectroMax","VivaEstilo","AutoNova","DulceCielo","VivaNaturaleza","TuCasaIdeal","SolucionesRápidas","MágicoReloj","EnigmaTecnológico","PlanetaVerde","EcoVibes","EstiloUrbano","DúoPerfecto","LunaViva","BrisaFresca","KiloSmart","TopOportunidades","PinturasMágicas","VueltaAlColección","LíneaRápida","ColorSplash","SúperFácil","RápidoYJusto","FitnessPro","JuguetesMágicos","ComidaDeliciosa","GoVerde","Salud360","ViajePerfecto","TuFuturoOnline","RelojPerfecto","MundoAmigos","AventuraPlus","TiemposFelices","EstiloElegante","¡VamosRápido!","ExperienciaÚnica","VelocidadPura","TodoEnUno","EcoRápido","MasterClases","SolucionesInstantáneas","LaFábricaDeSueños","MundoFuturo","MagiaEnCasa","MiVidaSana","SuperEnergía","LíneaDorada","VuelaAlto","MundoVirtual","TuMejorMomento","TodoPorTi","SolucionesPerfectas","MenteFeliz","ReposoTotal","CaféMágico","TuProgreso","EstiloEnCasa","TuÉxitoSeguro","JuntosEsMejor","SolucionesPro","MundoDeSueños","EstiloFresco","ComidaExpress","HogarDulceHogar","TiempoDeOportunidades","BrillaAhora","MundoFeliz","FórmulaMágica","RápidoYSeguro","AuténticoSabor","CrecimientoFácil","TuFuerza","MomentosPerfectos","CuidadoTotal","SaborReal","RápidoYNuevo","SolucionesAmigables","BienestarInmediato","HéroesEnCasa","MentePositiva","DíasFelices","CaféVital","EnergíaIntensa","HogarBrillante","ConectadosSiempre","¡RápidoAlÉxito!","MundoGourmet","VelocidadTotal","PlanetaMagia","EstiloClaro","EquilibrioPerfecto","TuOportunidad","SaborExclusivo","ComodidadTotal","DesafíoEnCasa","MañanasFelices","MenteCreativa","ÁgilComercio","NuevaEra","EquilibrioRápido","SolucionesExpress","MundoFresco","TodoParaTi","BienestarSeguro","VibranteEstilo","FamiliaFeliz","ComidaViva","TodoPerfecto","RápidoYFresco","JuguetesEstrella","AventuraFamiliar","RecetasFáciles","EfectoRápido","ViajeSeguro","EstiloVibrante","SolucionesIdeales","SaborNatural","ElectroNuevo","InnovaciónRápida","EnergíaVital","TuCasaFácil","MundoImparable","PlanetaFeliz","BrisaTotal","LlamaAventura","LucesBrillantes","DiversiónTotal","FábricaDeIdeas","ExperienciaInigualable","SolucionesIncreíbles","EscapadaIdeal","EstiloFácil","ComerSaludable","MagiaDeViaje","!RápidoYFácil","HogarNatural","EcoLuz","CieloFresco","VelocidadTotal","¡VamosALoGrande!","TuNuevoComienzo","ImaginaMás","ViajeMágico","NuevaPerspectiva","SonrisaSana","SolucionesGeniales","MundoMaravilloso","BrillaConFuerza","TodoPerfectoParaTi","NaturalezaEnCasa","ViajeIdeal","MomentosMágicos","EstiloPuro","ComidaPlena","SoloLoMejor","SuperSalud","ConectadosSiempre","EstiloInconfundible","MundoActivo","¡DisfrutaMás!","BellezaRadiante","MaravillasDiarias","JuguetesFeliz","EnergíaImparable","VibranteEstilo","RápidoYSeguro","CuidadoTotal","HogarIdeal","SolucionesVivas","CaféMaravilloso","EstiloUnico","UniversoSalud","TuTiempoFeliz","RecetasFrescas","EcoInnovador","FuerzaVital","TodoEstáBien","MagiaDeEstilo","EficienciaTotal","¡SéUnHéroe!","SonrisaRápida","MundoSostenible","EstiloGourmet","CrecimientoNatural","ComodidadFácil","InnovaciónDiaria","ViajePerfecto","CasaFeliz","SoloParaTi","JuguetesGeniales","MagiaViva","BrillaConEstilo","DescubreMás","ViajeIncreíble","EncuentraTodo"};
+		//Inmoviliaria
+		String[] nombreInmoviliaria = {};
+		String[] categorias = {"Regional","Nacional","Provincial","Internacional"};
+		String paisSede = "España";
+		/*nº oficinas */ 
+
+		//oficina
+		String[] direccion1={"Calle","Avenida","Paseo","Plaza","Bulevard",};
+		String[] direccion2={"Adrián", "Beatriz", "Carlos", "Daniela", "Elena","Francisco", "Gabriela", "Hugo", "Isabel", "Javier","Karla", "Luis", "Marta", "Nicolás", "Macedonia","Pablo", "Raquel", "Samuel", "Teresa", "Víctor","Alicia", "Gavilla", "Clara", "Diego", "Eva","Félix", "Gloria", "Héctor", "Irene", "Joaquín"};
+		/* cp generar 5 numeros */
+		String[] localidad={"Valladolid","Barcelona","Valencia","Vezdemarban","Alcalá de Henares", "Aínsa", "Albarracín", "Cangas de Onís", "Comillas","Cuenca", "Frigiliana", "Laguardia", "Olite", "Potes","Ronda", "Santillana del Mar", "Trujillo", "Úbeda", "Zafra"};
+		String[] provincias = {"Álava", "Albacete", "Alicante", "Almería", "Asturias","Ávila", "Badajoz", "Baleares", "Barcelona", "Burgos","Cáceres", "Cádiz", "Cantabria", "Castellón", "Ciudad Real","Córdoba", "Cuenca", "Gerona", "Granada", "Guadalajara","Guipúzcoa", "Huelva", "Huesca", "Jaén", "La Coruña","La Rioja", "Las Palmas", "León", "Lérida", "Lugo","Madrid", "Málaga", "Murcia", "Navarra", "Orense","Palencia", "Pontevedra", "Salamanca", "Santa Cruz de Tenerife", "Segovia","Sevilla", "Soria", "Tarragona", "Teruel", "Toledo","Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
+
+		//Persona
+		/*generar 8 num y 1 char */
+		/*utilizar direccion 2 */
+		/* urar provincia y localidad */
+		/*DNI generar 9 numerosw */
+
+
 		
-		String[] categorias = {"Resdencial","Comerciale","De Terreno","De Corporativas","De Renta","Internacional"};
-		
-		String pais = "España";
-		
-	
 		
 		
 	}
